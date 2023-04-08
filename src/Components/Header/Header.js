7b75065df024c5b8a50baea4c8dbf0258bdec029
dilip p m <dilippm92@gmail.com>
2023-04-08 16:48:10 +0530

    olx initial commit

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,12 +1,16 @@
-import React from 'react';
-
+import React, { useContext } from 'react';
+import { useHistory } from "react-router-dom";
 import './Header.css';
 import OlxLogo from '../../assets/OlxLogo';
 import Search from '../../assets/Search';
 import Arrow from '../../assets/Arrow';
 import SellButton from '../../assets/SellButton';
 import SellButtonPlus from '../../assets/SellButtonPlus';
+import { AuthContext, FirebaseContext } from '../../store/Context';
 function Header() {
+  const history = useHistory()
+  const { user } = useContext(AuthContext)
+  const { firebase } = useContext(FirebaseContext)
   return (
     <div className="headerParentDiv">
       <div className="headerChildDiv">
@@ -20,25 +24,48 @@ function Header() {
         </div>
         <div className="productSearch">
           <div className="input">
-            <input
-              type="text"
-              placeholder="Find car,mobile phone and more..."
-            />
+            <input type="text" placeholder="Find car,mobile phone and more..." />
           </div>
           <div className="searchAction">
             <Search color="#ffffff"></Search>
           </div>
         </div>
         <div className="language">
-          <span> ENGLISH </span>
+          <span>
+            ENGLISH
+          </span>
           <Arrow></Arrow>
         </div>
-        <div className="loginPage">
-          <span>Login</span>
+        <div
+          className="loginPage"
+          onClick={() => {
+            if (!user) {
+              firebase
+                .auth()
+                .signOut();
+              history.push('/login');
+            } 
+          }}>
+          <span>{
+            user
+              ? user.displayName
+              : 'Login'
+          }</span>
           <hr />
         </div>
+        {
+          user && <span
+            onClick={() => {
+              firebase
+                .auth()
+                .signOut()
+              history.push('/login')
+            }}>Logout</span>
+        }
 
-        <div className="sellMenu">
+        <div className="sellMenu" onClick={()=>{
+           history.push('/create');
+        }}>
           <SellButton></SellButton>
           <div className="sellMenuContent">
             <SellButtonPlus></SellButtonPlus>
